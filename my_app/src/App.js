be8c6960e964780ed16b7b328a40f2215cfd9957
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-08-09 21:57:50 -0700

    Created state called input and function that updates input and passed them as props to <Buttons />

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons from './components/Buttons';
 
 const App = () => {
+  const [input, setInput] = useState('');
+
+  const updateInput = n => {
+    setInput(input + n);
+  };
+
   return (
     <div className="container">
-      <Buttons />
+      <Buttons input={input} updateInput={updateInput} />
     </div>
   );
 };
