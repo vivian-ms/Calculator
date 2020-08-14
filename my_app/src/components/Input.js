4b75e2c3bd069bf49da18f61832b79d1e153d818
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-08-13 22:37:11 -0700

    Updated ternary operator

--- my_app/src/components/Input.js
+++ my_app/src/components/Input.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Input = ({ input }) => {
+const Input = ({ input, formula }) => {
   return (
     <div id="input">
-      {input ? input : '0'}
+      {/* If input exists: show input */}
+      {/* If input doesn't exist: if formula doesn't exist show zero in input; if formula exists show nothing in input */}
+      {input ? input : !formula ? '0' : null}
     </div>
   );
 };
