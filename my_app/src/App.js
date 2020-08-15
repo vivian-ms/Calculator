a3b05563e5f19b24b46811f84a37bc0a043f981b
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-08-14 22:51:09 -0700

    Combined all functions into one that uses switch statement and passed it as prop to <Button />

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -9,63 +9,80 @@ const App = () => {
   const [formula, setFormula] = useState('');
   const [answer, setAnswer] = useState('');
 
-  const updateInput = n => {
-    setInput(input + n);
-  };
+  const calc = (action, x = "") => {
+    switch (action) {
+      case "UPDATE_INPUT":
+        if (x === "clear") {
+          setInput("");
+        } else {
+          setInput(input + x);
+        }
+        break;
 
-  const updateFormula = f => {
-    /* ################################################## */
-    /* #### If formula is empty or ends with a number and an operator is pressed #### */
-    /* ################################################## */
+      case "UPDATE_FORMULA":
+        if (x === "clear") {
+          setFormula("");
+        } else {
+          /* ################################################## */
+          /* #### If formula is empty or ends with a number and an operator is pressed #### */
+          /* ################################################## */
 
-      // If operator is pressed before any number is pressed, formula starts with 0
-    if (!input && !formula && !answer) {
-      setFormula(`0 ${f}`);
+              // If operator is pressed before any number is pressed, formula starts with 0
+          if (!input && !formula && !answer) {
+            setFormula(`0 ${x}`);
 
-      // If a previous calculation was perfomed, add operator to the result 
-    } else if (!formula && answer) {
-      setFormula(`${answer} ${f}`);
-      setAnswer('');
+              // If a previous calculation was perfomed, add operator to the result
+          } else if (!formula && answer) {
+            setFormula(`${answer} ${x}`);
+            setAnswer("");
 
-      // Else add input number and operator to formula
-    } else if (input) {
-      setFormula(`${formula} ${input} ${f}`);
-      setInput('');
+              // Else add input number and operator to formula
+          } else if (input) {
+            setFormula(`${formula} ${input} ${x}`);
+            setInput("");
+          }
+
+          /* ################################################## */
+          /* ####### If formula ends with an operator and an operator is pressed ####### */
+          /* ################################################## */
+          if (!input && /[+*/-]/.test(formula[formula.length - 1])) {
+                  // If new operator is -, append - after existing operator
+            if (x === "-" && /\d/.test(formula[formula.length - 3])) {
+              setFormula(`${formula} ${x}`);
+
+                  // If formula ends with - and new operator isn't -, replace existing operator
+            } else if (/-/.test(formula[formula.length - 1]) && /[+*/]/.test(x)) {
+              setFormula(`${formula.substring(0, formula.length - 3).trim()} ${x}`);
+
+                  // If new operator isn't -, replace existing operator
+            } else {
+              setFormula(`${formula.substring(0, formula.length - 1).trim()} ${x}`);
+            }
+          }
+        }
+        break;
+
+      case "UPDATE_ANSWER":
+        if (x === "clear") {
+          setAnswer("");
+        } else {
+              // If there's input, add input to formula and solve
+          if (input) {
+            setAnswer(eval(`${formula} ${input}`));
+          }
+
+              // If there's a formula but no input, remove the operator and solve the formula
+          if (formula && !input) {
+            setAnswer(eval(`${formula.substring(0, formula.length - 1).trim()}`));
+          }
+
+          setInput("");
+          setFormula("");
+        }
+        break;
+
+        // No default
     }
-
-
-    /* ################################################## */
-    /* ####### If formula ends with an operator and an operator is pressed ####### */
-    /* ################################################## */
-    if (!input && /[+*/-]/.test(formula[formula.length - 1])) {
-      // If new operator is -, append - after existing operator
-      if (f === '-' && /\d/.test(formula[formula.length - 3])) {
-        setFormula(`${formula} ${f}`);
-
-        // If formula ends with - and new operator isn't -, replace existing operator
-      } else if (/-/.test(formula[formula.length - 1]) && /[+*/]/.test(f)) {
-        setFormula(`${formula.substring(0, formula.length - 3).trim()} ${f}`);
-
-        // If new operator isn't -, replace existing operator
-      } else {
-        setFormula(`${formula.substring(0, formula.length - 1).trim()} ${f}`);
-      }
-    }
-  };
-
-  const updateAnswer = () => {
-    // If there's input, add input to formula and solve
-    if (input) {
-      setAnswer(eval(`${formula} ${input}`));
-    }
-
-    // If formula ends with an operator and no input, remove the operator and solve the formula
-    if (formula && !input) {
-      setAnswer(eval(`${formula.substring(0, formula.length - 1).trim()}`));
-    }
-
-    setInput('');
-    setFormula('');
   };
 
   return (
@@ -75,7 +92,7 @@ const App = () => {
         { !answer && <Input input={input} formula={formula} answer={answer} /> }
         <Answer answer={answer} />
       </div>
-      <Buttons input={input} updateInput={updateInput} setInput={setInput} updateFormula={updateFormula} setFormula={setFormula} answer={answer} updateAnswer={updateAnswer} setAnswer={setAnswer} />
+      <Buttons input={input} answer={answer} calc={calc} />
     </div>
   );
 };
