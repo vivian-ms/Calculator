75d95e5cf8c3efe4f3c37294b527fe738153ea50
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-09-30 23:24:20 -0700

    Added class to dynamically position input

--- my_app/src/components/Input.js
+++ my_app/src/components/Input.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Input = ({ input, formula }) => {
   return (
-    <div id="input">
-      {/* If input exists: show input */}
-      {/* If input doesn't exist: if formula doesn't exist show zero in input; if formula exists show nothing in input */}
+      // If formula doesn't exist, add .down to shift input down; otherwise, remove .down
+    <div id="input" className={`position-relative ${!formula ? "down" : ""}`}>
+      {/* If input exists, show input */}
+      {/* If input doesn't exist & formula doesn't exist, show zero in input */}
+      {/* If input doesn't exist & formula exists, show nothing */}
       {input ? input : !formula ? '0' : null}
     </div>
   );
