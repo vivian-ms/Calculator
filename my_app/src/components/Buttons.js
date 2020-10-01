648b100f72e4900a101f438a228e8b4107ab0fb7
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-09-30 23:29:59 -0700

    Changed bootstrap classes to color group buttons based on functionality

--- my_app/src/components/Buttons.js
+++ my_app/src/components/Buttons.js
@@ -95,24 +95,24 @@ const Buttons = ({ input, answer, calc }) => {
   }, []);  // End useEffect
 
   return (
-    <div id="calculator">
-      <button type="button" id="zero" className="number btn btn-outline-dark" value="0" onClick={handleClick}>0</button>
-      <button type="button" id="one" className="number btn btn-outline-dark" value="1" onClick={handleClick}>1</button>
-      <button type="button" id="two" className="number btn btn-outline-dark" value="2" onClick={handleClick}>2</button>
-      <button type="button" id="three" className="number btn btn-outline-dark" value="3" onClick={handleClick}>3</button>
-      <button type="button" id="four" className="number btn btn-outline-dark" value="4" onClick={handleClick}>4</button>
-      <button type="button" id="five" className="number btn btn-outline-dark" value="5" onClick={handleClick}>5</button>
-      <button type="button" id="six" className="number btn btn-outline-dark" value="6" onClick={handleClick}>6</button>
-      <button type="button" id="seven" className="number btn btn-outline-dark" value="7" onClick={handleClick}>7</button>
-      <button type="button" id="eight" className="number btn btn-outline-dark" value="8" onClick={handleClick}>8</button>
-      <button type="button" id="nine" className="number btn btn-outline-dark" value="9" onClick={handleClick}>9</button>
-      <button type="button" id="decimal" className="btn btn-outline-dark" value="." onClick={handleClick}>.</button>
-      <button type="button" id="add" className="operator btn btn-outline-dark" value="+" onClick={handleClick}>+</button>
-      <button type="button" id="subtract" className="operator btn btn-outline-dark" value="-" onClick={handleClick}>-</button>
-      <button type="button" id="multiply" className="operator btn btn-outline-dark" value="*" onClick={handleClick}>*</button>
-      <button type="button" id="divide" className="operator btn btn-outline-dark" value="/" onClick={handleClick}>/</button>
-      <button type="button" id="equals" className="btn btn-outline-dark" onClick={handleClick}>=</button>
-      <button type="button" id="clear" className="btn btn-outline-dark" onClick={handleClick}>C</button>
+    <div id="buttons" className="mt-2">
+      <button type="button" id="zero" className="number btn btn-dark" value="0" onClick={handleClick}>0</button>
+      <button type="button" id="one" className="number btn btn-dark" value="1" onClick={handleClick}>1</button>
+      <button type="button" id="two" className="number btn btn-dark" value="2" onClick={handleClick}>2</button>
+      <button type="button" id="three" className="number btn btn-dark" value="3" onClick={handleClick}>3</button>
+      <button type="button" id="four" className="number btn btn-dark" value="4" onClick={handleClick}>4</button>
+      <button type="button" id="five" className="number btn btn-dark" value="5" onClick={handleClick}>5</button>
+      <button type="button" id="six" className="number btn btn-dark" value="6" onClick={handleClick}>6</button>
+      <button type="button" id="seven" className="number btn btn-dark" value="7" onClick={handleClick}>7</button>
+      <button type="button" id="eight" className="number btn btn-dark" value="8" onClick={handleClick}>8</button>
+      <button type="button" id="nine" className="number btn btn-dark" value="9" onClick={handleClick}>9</button>
+      <button type="button" id="decimal" className="btn btn-dark" value="." onClick={handleClick}>.</button>
+      <button type="button" id="add" className="operator btn btn-secondary" value="+" onClick={handleClick}>+</button>
+      <button type="button" id="subtract" className="operator btn btn-secondary" value="-" onClick={handleClick}>-</button>
+      <button type="button" id="multiply" className="operator btn btn-secondary" value="*" onClick={handleClick}>*</button>
+      <button type="button" id="divide" className="operator btn btn-secondary" value="/" onClick={handleClick}>/</button>
+      <button type="button" id="equals" className="btn btn-primary" onClick={handleClick}>=</button>
+      <button type="button" id="clear" className="btn btn-danger" onClick={handleClick}>C</button>
     </div>
   );
 };
