2b6accbc586470d0e59a379a977c8aad181896db
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-11-01 00:01:11 -0700

    Added left/right arrows that'll appear when formula > 25 digits

--- my_app/src/components/Formula.js
+++ my_app/src/components/Formula.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Formula = ({ formula }) => {
   return (
-    <div id="formula">
-      {formula ? formula : null}
+    <div id="formula" className="position-relative">
+      {/* Show arrows to scroll if formula is longer than 25 digits */}
+      { formula.length > 25 && <><i className="fas fa-angle-left position-absolute"></i><i className="fas fa-angle-right position-absolute"></i></> }
+      { formula ? <p className={`${formula.length > 25 ? "pl-2 pr-3" : ""}`}> {formula} </p> : null }
     </div>
   );
 };
