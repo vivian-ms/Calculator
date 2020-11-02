cf9b6444419953cbb3af426b9589fdfc03f39ccd
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-11-01 23:15:05 -0800

    Added code to scroll formula using left/right arrows

--- my_app/src/components/Formula.js
+++ my_app/src/components/Formula.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 const Formula = ({ formula }) => {
+  const scrollLeft = () => {
+    document.querySelector('#formula').scrollLeft -= 30;
+  }
+
+  const scrollRight = () => {
+    document.querySelector('#formula').scrollLeft += 30;
+  }
+
   return (
-    <div id="formula" className="position-relative">
-      {/* Show arrows to scroll if formula is longer than 25 digits */}
-      { formula.length > 25 && <><i className="fas fa-angle-left position-absolute"></i><i className="fas fa-angle-right position-absolute"></i></> }
-      { formula ? <p className={`${formula.length > 25 ? "pl-2 pr-3" : ""}`}> {formula} </p> : null }
+    <div id="formula-container" className="position-relative">
+      {/* Show arrows to scroll if formula > 25 digits */}
+      { formula.length > 25 &&
+        <>
+          <i className="fas fa-angle-left position-fixed" onClick={scrollLeft}></i>
+          <i className="fas fa-angle-right position-fixed" onClick={scrollRight}></i>
+        </>
+      }
+
+      {/* Add left/right margin if formula > 25 digits */}
+      { formula ?
+        <div id="formula" className={`${formula.length > 25 ? "ml-2 mr-3" : ""}`}>
+          <p>{formula}</p>
+        </div> : null }
     </div>
   );
 };
