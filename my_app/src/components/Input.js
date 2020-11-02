033cce03251cd7e46295dbac4205051d05a1e6e4
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-11-01 22:29:28 -0800

    Removed class that was used to shift input down if there was no formula

--- my_app/src/components/Input.js
+++ my_app/src/components/Input.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Input = ({ input, formula }) => {
   return (
-      // If formula doesn't exist, add .down to shift input down; otherwise, remove .down
-    <div id="input" className={`position-relative ${!formula ? "down" : ""}`}>
+    <div id="input">
       {/* If input exists, show input */}
-      {/* If input doesn't exist & formula doesn't exist, show zero in input */}
-      {/* If input doesn't exist & formula exists, show nothing */}
+      {/* If input doesn't exist & formula doesn't exist: show zero in input */}
+      {/* If input doesn't exist & formula exists:         show nothing */}
       {input ? input : !formula ? '0' : null}
     </div>
   );
