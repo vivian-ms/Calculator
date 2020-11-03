2a1079371d15dac47d4f807beca33dffd30e5d41
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-11-02 22:25:48 -0800

    Added code that'll show answer in exponential notation if > 10 digits

--- my_app/src/components/Answer.js
+++ my_app/src/components/Answer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Answer = ({ answer }) => {
   return (
     <div id="answer" className="position-relative down">
-      {answer ? answer : null}
+      {/* If answer > 10 digits, show answer in exponential notation */}
+      { !answer ?
+      null : answer.toString().length > 10 ?
+        answer.toExponential(5) : answer }
     </div>
   );
 };
